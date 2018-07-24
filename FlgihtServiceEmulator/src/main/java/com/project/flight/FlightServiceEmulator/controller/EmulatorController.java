676d0c24be7e3f91d7f711f56c75db3088854b33
Patrycja Zań <patizan@interia.pl>
2018-07-24 10:58:57 +0200

    Add RestController

--- FlgihtServiceEmulator/src/main/java/com/project/flight/FlightServiceEmulator/controller/EmulatorController.java
+++ FlgihtServiceEmulator/src/main/java/com/project/flight/FlightServiceEmulator/controller/EmulatorController.java
@@ -1,11 +1,14 @@
 package com.project.flight.FlightServiceEmulator.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
+@RestController
+@RequestMapping("/api/flights")
 public class EmulatorController {
     private final TaskScheduler scheduledEmulator;
     private final RestTemplate restTemplate;
@@ -17,7 +20,9 @@ public class EmulatorController {
     }
 
     @RequestMapping(value = "/{id}/start", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
     public void startFlight(@PathVariable("id") String id) {
+        log.debug("startFlight");
         scheduledEmulator.addTask(id);
     }
 
