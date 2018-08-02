85a2e76c562f53dd358191fcad2a990671542d9b
Patrycja Zań <patizan@interia.pl>
2018-08-02 14:01:59 +0200

    Add @CrossOrigin

--- FlightServiceEmulator/src/main/java/com/project/flight/FlightServiceEmulator/controller/EmulatorController.java
+++ FlightServiceEmulator/src/main/java/com/project/flight/FlightServiceEmulator/controller/EmulatorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("/api/flights")
 public class EmulatorController {
